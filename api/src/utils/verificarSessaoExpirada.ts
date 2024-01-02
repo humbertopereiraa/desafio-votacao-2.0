2fd73f5ab0dd807c6b548e71d793b8888d7fb7cb
Humberto <humbertopereira_15@hotmail.com>
2024-01-01 22:42:53 -0300

    Correção na função de verificar sessão expirada

--- api/src/utils/verificarSessaoExpirada.ts
+++ api/src/utils/verificarSessaoExpirada.ts
@@ -1,8 +1,9 @@
 /**Função para verificar se um sessão de Pauta expirou
  * @description Se a Data da criação da Pauta mais(+) Tempo da Sessão for menor(<) que a Data atual, a sessão expirou
  */
-export function sessaoExpirou(dataDaPauta: Date, tempoSessao: number) {
-  const novaData = new Date(dataDaPauta.getTime() + tempoSessao * 60000)
+export function sessaoExpirou(dataDaPauta: Date | number, tempoSessao: number) {
+  const dataDaCriacao = new Date(dataDaPauta)
+  const novaData = new Date(dataDaCriacao.getTime() + tempoSessao * 60000)
   const dataAtual = new Date()
   return novaData.valueOf() < dataAtual.valueOf()
 } 
